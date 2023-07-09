6f5b447a588ee12bf659185cd589df111567e6d5
Mateusz Budny <mateusz.budny97@gmail.com>
2023-07-09 19:57:25 +0200

    fixes

--- Assets/_GMTK2023/Scripts/Shooter.cs
+++ Assets/_GMTK2023/Scripts/Shooter.cs
@@ -19,6 +19,8 @@ public class Shooter : MonoBehaviour
     private float maxRotationSpeed = 1f;
     [SerializeField]
     private Transform ballSpawnPoint;
+    [SerializeField]
+    private GameObject heart;
 
     [BoxGroup("Starting Modules")]
     [SerializeField]
