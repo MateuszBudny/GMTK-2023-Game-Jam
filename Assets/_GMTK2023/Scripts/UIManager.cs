b90ac32aecabc46772f827eb77adc54351aa7089
Mateusz Budny <mateusz.budny97@gmail.com>
2023-07-09 18:33:04 +0200

    Basic UI added (win. lose)

---
+++ Assets/_GMTK2023/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : SingleBehaviour<UIManager>
+{
+    [SerializeField]
+    private GameObject youLoseScreen;
+    [SerializeField]
+    private GameObject youWinScreen;
+    [SerializeField]
+    private string nextLvlName;
+
+    public void OnYouLose()
+    {
+        youLoseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void YouLoseClick()
+    {
+        RestartManager.Instance.Restart();
+    }
+
+    public void OnYouWin()
+    {
+        youWinScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void YouWinClick()
+    {
+        SceneManager.LoadScene(nextLvlName);
+    }
+}
